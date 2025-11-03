8921bdc1a96308ff99deba9fa4a8f9d8002d4e9c
lopeznoval <uo282182@uniovi.es>
2025-11-03 20:06:18 +0100

    mas

--- LoRa/v1/parameters.py
+++ LoRa/v1/parameters.py
@@ -1,12 +1,13 @@
-SERIAL_PORT = "COM3"
+SERIAL_PORT = "/dev/ttyUSB1"
 
-SERIAL_PORT_ROBOT = None # "/dev/ttyACM0"  # or /dev/ttyUSB1 depending on your connection
-BAUDRATE_ROBOT = None # 115200
+SERIAL_PORT_ROBOT = "/dev/ttyACM0"  # or /dev/ttyUSB1 depending on your connection
+BAUDRATE_ROBOT = 115200
 
 FREQUENCY = 433          # MHz
-NODE_ADDRESS = 20        # Your node address
+NODE_ADDRESS = 10        # Your node address
 NODE_ADDRESS_DEST = 1    # Destination address
-RELAY_FLAG = 1            # Relay flag
+RELAY_FLAG = 0            # Relay flag
 TYPE_MSG = 1             # Message type
-ID_MSG = 69               # Message ID
+ID_MSG = 0               # Message ID
 POWER = 0                # dBm
+EB=0
